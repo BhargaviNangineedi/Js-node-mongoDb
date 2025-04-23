5a02fbc399176cce23a03fae94c0777e2a2ad593
BhargaviNangineedi <bhargave516@gmail.com>
2025-04-23 12:07:43 +0530

    Add files via upload
    Added initial project files”

---
+++ app.mjs
@@ -0,0 +1,8 @@
+/*const math=require("./math.mjs");
+console.log(math.add(5,3))
+console.log(math.sub(7,5))*/
+/*import{add, sub} from "./math.mjs";
+console.log(add(5,3))
+console.log(sub(9,6))*/
+console.log(0.1+0.2===0.3)
+console.log(Number((0.1+0.2).toFixed(10))===0.3)
